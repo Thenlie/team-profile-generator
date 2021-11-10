8a4cb66b26e396350233d78fc629d187b0c0eb4c
Leithen <leithen113@gmail.com>
2021-11-09 19:05:07 -0700

    Created engineer and intern prompt blueprint

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+
+const employees = [];
 
 //get manager information
 managerPrompt = function() {
@@ -6,21 +11,26 @@ managerPrompt = function() {
         .prompt([
             {
                 type: 'input',
-                name: 'managerName',
+                name: 'name',
                 message: "Please enter the team manager's name"
             }, {
                 type: 'input',
-                name: 'managerId',
+                name: 'id',
                 message: "Please enter the team manager's employee ID number"
             }, {
                 type: 'input',
-                name: 'managerEmail',
+                name: 'email',
                 message: "Please enter the team manager's email address"
+            }, {
+                type: 'input',
+                name: 'officeNum',
+                message: "Please enter the team manager's office number"
             }
         ])
         .then((answers) => {
-            console.log(answers);
-            //create a new manager object with answers
+            //create a new manager object with answers and push to employees array
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNum) 
+            employees.push(manager)
             //then run function to see if user wants to add more employees
             addEmployee();
         })
@@ -43,10 +53,79 @@ addEmployee = function() {
             if (answer.userChoice === 'Finish Building Team') {
                 //build the HTML file with current employees
                 console.log('Done!')
-            } else {
-                //run a function to create an engineer or intern
-                console.log('Adding more!')
+            } else if (answer.userChoice === 'Engineer') {
+                //run a function to create an engineer
+                engineerPrompt();
+            } else if (answer.userChoice === 'Intern') {
+                //run a function to create an intern
+                internPrompt();
             }
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+}
+
+//get engineer information
+engineerPrompt = function() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Please enter the engineer's name"
+            }, {
+                type: 'input',
+                name: 'id',
+                message: "Please enter the engineer's employee ID number"
+            }, {
+                type: 'input',
+                name: 'email',
+                message: "Please enter the engineer's email address"
+            }, {
+                type: 'input',
+                name: 'github',
+                message: "Please enter the engineer's GitHub username"
+            }
+        ])
+        .then((answers) => {
+            //create a new engineer object with answers and push to employees array
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.officeNum) 
+            //then run function to see if user wants to add more employees
+            addEmployee();
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+}
+
+//get intern information
+internPrompt = function() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Please enter the intern's name"
+            }, {
+                type: 'input',
+                name: 'id',
+                message: "Please enter the intern's employee ID number"
+            }, {
+                type: 'input',
+                name: 'email',
+                message: "Please enter the intern's email address"
+            }, {
+                type: 'input',
+                name: 'github',
+                message: "Please enter the intern's school name"
+            }
+        ])
+        .then((answers) => {
+            //create a new intern object with answers and push to employees array
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.officeNum) 
+            //then run function to see if user wants to add more employees
+            addEmployee();
         })
         .catch((error) => {
             console.log(error)
