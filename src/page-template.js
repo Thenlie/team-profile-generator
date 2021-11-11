8d1669e138e78fed43f69031ebab7a23011a48f6
Leithen <leithen113@gmail.com>
2021-11-10 17:47:28 -0700

    Updating styling

--- src/page-template.js
+++ src/page-template.js
@@ -3,31 +3,43 @@ generateCard = employee => {
     if (employee.constructor.name === 'Manager'){
         return `
         <section class='card'>
+            <div class='card-header'>
             <h2 class='name'>${employee.name}</h2>
-            <h2 class='title'>${employee.getRole()}</h2>
-            <div class='id'>Employee ID: ${employee.id}</div>
-            <div class='email'>Email <a href="mailto:${employee.email}">${employee.email}</a></div>
-            <div class='info'>Office Number: ${employee.officeNumber}</div>
+            <h2 class='title'><i class="bi bi-clipboard-check"></i> ${employee.getRole()}</h2>
+            </div>
+            <ul class='list-group list-group-flush'>
+            <li class='id list-group-item'>Employee ID: ${employee.id}</li>
+            <li class='email list-group-item'>Email <a href="mailto:${employee.email}">${employee.email}</a></li>
+            <li class='info list-group-item'>Office Number: ${employee.officeNumber}</li>
+            </ul>
         </section>
         `
     } else if (employee.constructor.name === 'Engineer'){
         return `
         <section class='card'>
+            <div class='card-header'>
             <h2 class='name'>${employee.name}</h2>
-            <h2 class='title'>${employee.getRole()}</h2>
-            <div class='id'>Employee ID: ${employee.id}</div>
-            <div class='email'>Email <a href="mailto:${employee.email}">${employee.email}</a></div>
-            <div class='info'>GitHub: <a href="${employee.getGithub()}"> ${employee.github} </a></div>
+            <h2 class='title'><i class="bi bi-wrench"></i> ${employee.getRole()}</h2>
+            </div>
+            <ul class='list-group list-group-flush'>
+            <li class='id list-group-item'>Employee ID: ${employee.id}</li>
+            <li class='email list-group-item'>Email <a href="mailto:${employee.email}">${employee.email}</a></li>
+            <li class='info list-group-item'>GitHub: <a href="${employee.getGithub()}"> ${employee.github} </a></li>
+            </ul>
         </section>
         `
     } else if (employee.constructor.name === 'Intern'){
         return `
         <section class='card'>
+            <div class='card-header'>
             <h2 class='name'>${employee.name}</h2>
-            <h2 class='title'>${employee.getRole()}</h2>
-            <div class='id'>Employee ID: ${employee.id}</div>
-            <div class='email'>Email <a href="mailto:${employee.email}">${employee.email}</a></div>
-            <div class='info'>School: ${employee.getSchool()}</div>
+            <h2 class='title'><i class="bi bi-mortarboard-fill"></i> ${employee.getRole()}</h2>
+            </div>
+            <ul class='list-group list-group-flush'>
+            <li class='id list-group-item'>Employee ID: ${employee.id}</li>
+            <li class='email list-group-item'>Email <a href="mailto:${employee.email}">${employee.email}</a></li>
+            <li class='info list-group-item'>School: ${employee.getSchool()}</li>
+            </ul>
         </section>
         `
     }
@@ -47,11 +59,16 @@ module.exports = employees => {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="/dist/styles.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+      <link rel="stylesheet" href="./styles.css">
       <title>Team Profile Generator</title>
     </head>
 
     <body>
+        <header>
+            <h1>My Team</h1>
+        </header>
         <main>
             ${cardArray}
         </main>
